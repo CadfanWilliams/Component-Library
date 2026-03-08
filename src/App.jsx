2375a984ddaea27e739de6486f1bfa906b79de0a
Cadfan <cadfanw@gmail.com>
2026-03-08 17:12:49 +0000

    started adding rating bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
+
 import Card from "./components/RatingCard/RatingCard";
 
 function App() {
   return (
     <>
-      <Card
-        title="EXCELLENT"
-        rating={4.4}
-        description="This is a simple card component."
-      />
+      <Card title="EXCELLENT" rating={4.4} />
     </>
   );
 }
--- src/components/RatingCard/RatingCard.jsx
+++ src/components/RatingCard/RatingCard.jsx
@@ -9,7 +9,6 @@ export default function Card({ title, description, rating }) {
         {title && <Title>{title}</Title>}
         <StarRating rating={rating} />
         {rating && <p className="card-rating">{rating} OUT OF 5</p>}
-        {description && <p className="card-description">{description}</p>}
         <p>Product Rating "feefo"</p>
       </div>
     </CardContainer>
